a8bb2af26b6a88774bdd51caf4382bb6c613fc1d
Voderm Li <390964835@qq.com>
2022-08-30 10:57:41 +0800

    “The second commit”

--- src/Voderm/learning/InputChar.java
+++ src/Voderm/learning/InputChar.java
@@ -7,7 +7,8 @@ package Voderm.learning;
 public class InputChar {
     public static void main(String[] args) {
         System.out.println("请输出一个字符");
-        char c = (char)System.in.read();
-        
+//        char c = (char)System.in.read();
+        //Java 异常类都是Throwable的子类。 Throwable类有两个子类，一个是Error类，另一个是Exception类
+
     }
 }
